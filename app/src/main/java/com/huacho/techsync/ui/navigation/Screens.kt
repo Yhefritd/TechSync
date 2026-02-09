74517cb6da7f19c1256a2dedb51c24057be95ff7
Yhefritd <huachomontes123.1@gmail.com>
2026-02-09 03:25:29 -0500

    feat: Creado el archivo Screens con la sealed_class (clase sellada)sobre nuestras pantallas que son limitadas

---
+++ app/src/main/java/com/huacho/techsync/ui/navigation/Screens.kt
@@ -0,0 +1,10 @@
+package com.huacho.techsync.ui.navigation
+
+sealed class Screens(val route: String) {
+    data object Login : Screens("login")
+    data object Register : Screens("register")
+    data object Dashboard : Screens("dashboard")
+    data object Formulario : Screens("formulario")
+    data object Detalle : Screens("detalle")
+
+}
